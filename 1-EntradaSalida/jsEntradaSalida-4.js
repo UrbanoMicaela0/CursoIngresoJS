56e30a5bf28ff50d01f6f09af59e10235c8c68c7
UrbanoMicaela0 <52429649+UrbanoMicaela0@users.noreply.github.com>
2019-07-01 22:19:21 -0300

    Add files via upload
    Clase Programación 01/07/2019

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,13 @@
 */
 function mostrar()
 {
+var nombre;
+nombre= prompt ("Ingrese su nombre: ");
+document.getElementById("elNombre").value =nombre;
+//enviar el prompt a la caja texto diferente a que la caja vaya al prompt
+//Se escribre igual pero la variable nombre esta a la derecha del igual
+
+
 
 }
 
