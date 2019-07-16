cb8e18d27780175ac53f33c82a484419047e1c5e
UrbanoMicaela0 <52429649+UrbanoMicaela0@users.noreply.github.com>
2019-07-16 18:57:49 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,8 +1,16 @@
 function mostrar()
+//tomo la edad
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño= document.getElementById('mes').value;
 
+  if (mesDelAño= enero){
+    alert("Que comiences bien el año"); }
+  elseif (mesDelAño= Marzo){
+    alert ("A clases!!!");}
+  elseif (mesDelAño= julio) {
+    alert ("se vienen las vacaciones");}
+  elseif ( mesDelAño=diciembre){
+    alert ("Felices fiestas") }
 
 
 
